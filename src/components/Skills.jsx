02b86e08ce72233ca442242c70ace1fb9c70f657
griffinmc22 <57164055+griffinmc22@users.noreply.github.com>
2023-04-11 21:20:31 -0700

    initial commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import FireBase from '../assets/firebase.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
+import { FaAws } from 'react-icons/fa';
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f]  text-gray-300 py-4'>
+        {/* container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+                <p className='py-4'>These are the thechnologies I've worked with</p>
+            </div>
+
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt='HTML Icon'></img>
+                    <p>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt='HTML Icon'></img>
+                    <p>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JavaScript} alt='HTML Icon'></img>
+                    <p>Javascript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={ReactImg} alt='HTML Icon'></img>
+                    <p>React</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={GitHub} alt='HTML Icon'></img>
+                    <p>Github</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Node} alt='HTML Icon'></img>
+                    <p>Node</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Mongo} alt='HTML Icon'></img>
+                    <p>MongoDB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={FireBase} alt='HTML Icon'></img>
+                    <p>Firebase</p>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Skills
