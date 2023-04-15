3ce7fadf540602404ffd96e762dbe5ea5bd715d9
griffinmc22 <57164055+griffinmc22@users.noreply.github.com>
2023-04-14 20:06:10 -0700

    changed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt='Logo Image'style={{width: '50px'}}/>
+            <img src={Logo} alt='Logo Image'style={{width: '100px'}}/>
         </div>
         {/* menu */}
            <ul className='hidden md:flex border-2 rounded-lg p-2 bg-[#0a192f]'>
