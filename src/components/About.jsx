c10e165bc73775acf714a45a59117c3aade98856
griffinmc22 <57164055+griffinmc22@users.noreply.github.com>
2023-04-22 22:11:42 -0700

    testing changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             </div>
                 <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                     <div className='sm:text-right text-4xl font-bold'>
-                        <p>Hi, nice to meet you.</p>
+                        <p>Hi, nice to meet you!</p>
                         <p>Please take a look around.</p>
                     </div>
                     <div>
